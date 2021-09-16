94cfc5bd358ac7664127e63a1467b95c9bea6bf7
Milos Simek <milos.simek@seznam.cz>
2021-09-16 14:16:42 +0200

    252. Modules and import

---
+++ 252. Modules and import.py
@@ -0,0 +1,14 @@
+from turtle import *
+
+done = "well done, you have finish your drawing"
+
+#from turtle import forward, right, done
+#import turtle
+
+forward(150)
+right(250)
+forward(250)
+circle(75)
+
+done()
+print(done)
