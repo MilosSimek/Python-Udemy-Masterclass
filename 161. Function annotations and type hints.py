a32ed34e151f91b1134bb66d3ee95f072f0de13f
Milos Simek <milos.simek@seznam.cz>
2021-09-17 11:15:26 +0200

    161. Function annotations and type hints

---
+++ 161. Function annotations and type hints.py
@@ -0,0 +1,22 @@
+def  fibobacci(n: int) -> int:
+    """
+    Return the `n` th Fibonnaci number, for positive `n`.
+    :param int: positive integer 
+    
+    """
+    if 0 <= n <= 1:
+        return n
+
+    n_minus1, n_minus2 = 1, 0
+
+    result = None
+    for f in range(n - 1):
+        result = n_minus2 + n_minus1
+        n_minus2 = n_minus1
+        n_minus1 = result
+
+    return result
+
+
+for i in range(36):
+    print(i, fibobacci(i))
